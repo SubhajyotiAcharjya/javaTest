d8d7b9a7fd486ebf902047eb59a8a8e150287144
SubhajyotiAcharjya <subhaacharjya2019@gmail.com>
2026-01-29 11:01:54 +0530

    Test.java

--- Test.java
+++ Test.java
@@ -30,8 +30,8 @@ public class Test {
 
     // Main method
     public static void main(String[] args) {
-        int x = 600;
-        int y = 10;
+        int x = 500;
+        int y = 25;
 
         add(x, y);
         sub(x, y);
